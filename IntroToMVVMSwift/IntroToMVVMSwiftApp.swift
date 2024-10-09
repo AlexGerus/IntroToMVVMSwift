adc0b4088f6f8b2c41603d117860873463bffe4b
Alex Gerus <alexander.gerus@grse.de>
2024-10-09 13:58:41 +0300

    Init

---
+++ IntroToMVVMSwift/IntroToMVVMSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToMVVMSwiftApp.swift
+//  IntroToMVVMSwift
+//
+//  Created by Alexander Gerus on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct IntroToMVVMSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerListView()
+        }
+    }
+}
---
+++ IntroToMVVMSwift/Views/LoadingView.swift
@@ -0,0 +1,27 @@
+//
+//  LoadingView.swift
+//  IntroToMVVMSwift
+//
+//  Created by Alexander Gerus on 24.04.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).edgesIgnoringSafeArea(.all)
+            
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                .scaleEffect(2)
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
